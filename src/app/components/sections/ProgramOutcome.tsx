913c9c5ab54c590885fca044b26a091c8d5830f8
rimsha88 <123818469+rimsha88@users.noreply.github.com>
2024-04-28 16:33:53 +0500

    Update ProgramOutcome.tsx

--- src/app/components/sections/ProgramOutcome.tsx
+++ src/app/components/sections/ProgramOutcome.tsx
@@ -24,12 +24,12 @@ const ProgramOutcome = () => {
               The Outcome for Participants of the Program
             </h2>
             <p className="mt-4 text-slate-600 text-lg">
-              As a graduate of this program, you will own valuable products such
+              {`As a graduate of this program, you will own valuable products such
               as Full-Stack App Templates, AR and VR Experiences, and APIs that
               are marketed globally by the Panaverse DAO. You will also have the
               opportunity to offer your services at a rate of $50 per hour,
               providing a path to financial stability while contributing to the
-              growth of Pakistan's software exports.
+              growth of Pakistan's software exports.`}
             </p>
             <div className="mt-6 grid grid-cols-2 gap-x-2 gap-y-4">
               {outcomesPoints.map((items, i) => (
